bf4229f785da7a78e32d85ded901809dde7e482b
KainGC <cows_say_potato@yahoo.com>
2012-10-16 13:02:14 -0700

    Updated versions so no clobber necessary
    Change-Id: Ibbe0e59fc56266f1a4af477c673310d8266ca551
    Signed-off-by: KainGC <cows_say_potato@yahoo.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -12,7 +12,7 @@ ifneq ($(AOKP_BUILD),)
 		ro.aokp.version=$(TARGET_PRODUCT)_jb_$(shell echo $(AOKP_BUILD) | cut -d/ -f2)
 else
 	PRODUCT_PROPERTY_OVERRIDES += \
-		ro.aokp.version=$(TARGET_PRODUCT)_unofficial_
+		ro.aokp.version=$(TARGET_PRODUCT)_unofficial
 endif
 
 # Camera shutter sound property
